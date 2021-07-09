2777dc830fab4dbb71abcad64f0ca162b61dda85
Darren12345677 <63776243+Darren12345677@users.noreply.github.com>
2021-07-09 23:51:04 +0800

    Delete median-of-two-sorted-arrays directory

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        combine = nums1 + nums2
-        combine.sort()
-        x = len(combine)
-        if x%2 == 0:
-            return (combine[x//2-1] + combine[x//2])/2
-        return combine[x//2]
